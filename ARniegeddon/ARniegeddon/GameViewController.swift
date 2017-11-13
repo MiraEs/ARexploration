d87f8cf2330bd1c4de7b29ee5f5c402c851b98ab
Ilmira Estil <ilmira@prolificinteractive.com>
2017-11-12 19:30:39 -0500

    initial setup, bug test

--- ARniegeddon/ARniegeddon/GameViewController.swift
+++ ARniegeddon/ARniegeddon/GameViewController.swift
@@ -28,31 +28,41 @@
  * THE SOFTWARE.
  */
 
-import UIKit
-import SpriteKit
-import GameplayKit
+import ARKit
 
 class GameViewController: UIViewController {
+
+  var sceneView: ARSKView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    if let view = self.view as! SKView? {
-      // Load the SKScene from 'GameScene.sks'
-      if let scene = SKScene(fileNamed: "GameScene") {
-        // Set the scale mode to scale to fit the window
-        scene.scaleMode = .aspectFill
-        
-        // Present the scene
-        view.presentScene(scene)
-      }
+    if let view = self.view as! ARSKView? {
+      sceneView = view
+      sceneView!.delegate = self
       
-      view.ignoresSiblingOrder = true
+      let scene = GameScene(size: view.bounds.size)
+      scene.scaleMode = .resizeFill
+      scene.anchorPoint = CGPoint(x: 0.5, y: 0.5) //?
       
-      view.showsFPS = true
+      view.presentScene(scene)
+      view.showsFPS = false
       view.showsNodeCount = true
     }
   }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    let configuration = ARWorldTrackingConfiguration()
+    sceneView.session.run(configuration)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    sceneView.session.pause()
+  }
+  
+  //MARK:
   
   override var shouldAutorotate: Bool {
     return true
@@ -66,12 +76,30 @@ class GameViewController: UIViewController {
     }
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Release any cached data, images, etc that aren't in use.
-  }
   
   override var prefersStatusBarHidden: Bool {
     return true
+  }
+}
+
+extension GameViewController: ARSKViewDelegate {
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    print("Session fail")
+  }
+  
+  func sessionWasInterrupted(_ session: ARSession) {
+    print("Session interruption")
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    print("Session resumed")
+    sceneView.session.run(session.configuration!, options: [.resetTracking, .removeExistingAnchors])
+  }
+  
+  func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+    let bug = SKSpriteNode(imageNamed: "bug")
+    bug.name = "bug"
+    
+    return bug
   }
 }
